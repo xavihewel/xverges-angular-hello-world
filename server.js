169efde26e6daee3ccd21977e00a962e3c261ae0
Xavier Vergés <xavier.verges@bonarea.com>
2022-03-14 11:35:35 +0100

    HOTFIX: arreglar server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/xverges-angular-hell-world'));
+app.use(express.static(__dirname + '/dist/xverges-angular-hello-world'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/xverges-angular-hell-world/index.html'));});
+'/dist/xverges-angular-hello-world/index.html'));});
 app.listen(process.env.PORT || 8080);
