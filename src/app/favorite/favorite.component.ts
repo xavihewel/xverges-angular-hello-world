01cfa6711a8afce5914d6678cf02055c54c6c021
Xavier Vergés <xavier.verges@bonarea.com>
2022-03-16 10:14:42 +0100

    afegir notificacions

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-favorite',
@@ -10,6 +11,47 @@ export class FavoriteComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log('this is init method');
   }
 
+  simpleAlert() {
+    Swal.fire('Hell world');
+  }
+
+  alertWithSuccesss() {
+    Swal.fire('Thank you...', 'You submitted succesfully!', 'success');
+  }
+
+  confirmBox() {
+    Swal.fire({
+      title: 'Are you sure want to remove?',
+      text: 'You will not be able to recover this file!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it',
+      cancelButtonText: 'No, keep it'
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire(
+          'Deleted!',
+          'Your imaginary file has been deleted',
+          'success'
+        )
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire(
+          'Cancellled',
+          'Your imaginary file is safe',
+          'error'
+        )
+      }
+    })
+  }
+  erroralert(){
+    Swal.fire({
+      icon: 'error',
+      title: 'Ooops..',
+      text: 'Something went wrong!',
+      footer: '<a href>Why do I have this issue?</a>'
+    })
+  }
 }
